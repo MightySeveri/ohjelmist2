65b028b9b6b87f3d180bda31a99b643f032864ec
MightySeveri <severi.kerkka@gmail.com>
2025-04-03 09:44:22 +0300

    sitoutumis viesti

--- javascript/moduuli1/t6.js
+++ javascript/moduuli1/t6.js
@@ -1,8 +1,14 @@
-const answer = confirm('Should i calculate the square root')
-    if( answer = true);
-        num = prompt('syötä numero');
-        sqr = num / num
-        alert(sqr)
-    else{
-        alert('the square will not be calculated')
+const answer = confirm('Should I calculate the square root?');
+
+if (answer) {
+    let num = Number(prompt('Syötä numero'));
+
+    if (num < 0) {
+        alert('Neliöjuurta ei voi laskea negatiiviselle luvulle.');
+    } else {
+        let sqr = Math.sqrt(num);
+        alert(sqr);
+    }
+} else {
+    alert('The square root will not be calculated.');
 }
