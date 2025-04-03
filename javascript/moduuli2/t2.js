5adf433776d967dc7f0f5125e8666772a4176dce
MightySeveri <severi.kerkka@gmail.com>
2025-04-03 11:20:32 +0300

    sitoutumis viesti

--- javascript/moduuli2/t2.js
+++ javascript/moduuli2/t2.js
@@ -1,6 +1,6 @@
 osallistujat = prompt('number of participants')
 nimi = []
 for(let i = 0; i < osallistujat; i++) {
-   nimi= prompt('osallistujan nimi');
+  nimi= prompt('osallistujan nimi');
 }
 alert(nimi);
